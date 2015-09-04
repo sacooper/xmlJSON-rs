d0c54c33fa72d094ff7c8eb3de2c394baff03f94
Scott Cooper <scott.cooper2@mail.mcgill.ca>
2015-09-04 09:35:40 -0400

    Update

--- implementors/rustc_serialize/json/trait.ToJson.js
+++ implementors/rustc_serialize/json/trait.ToJson.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rustc_serialize'] = [];implementors['xmlJSON'] = ["impl <a class='trait' href='rustc_serialize/json/trait.ToJson.html' title='rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='xmlJSON/struct.XmlDocument.html' title='xmlJSON::XmlDocument'>XmlDocument</a>",];implementors['xmlJSON'] = ["impl <a class='trait' href='rustc_serialize/json/trait.ToJson.html' title='rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='xmlJSON/struct.XmlDocument.html' title='xmlJSON::XmlDocument'>XmlDocument</a>",];implementors['xmlJSON'] = ["impl <a class='trait' href='rustc_serialize/json/trait.ToJson.html' title='rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='xmlJSON/struct.XmlDocument.html' title='xmlJSON::XmlDocument'>XmlDocument</a>",];
+implementors['rustc_serialize'] = [];implementors['xmlJSON'] = ["impl <a class='trait' href='rustc_serialize/json/trait.ToJson.html' title='rustc_serialize::json::ToJson'>ToJson</a> for <a class='struct' href='xmlJSON/struct.XmlDocument.html' title='xmlJSON::XmlDocument'>XmlDocument</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
